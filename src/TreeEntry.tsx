c6ce25037d2f6109deed205fa06425af1fcaebdc
Henrique Doro <opensource@hdoro.dev>
2021-10-21 11:20:04 -0300

    style: highlight open document

--- src/TreeEntry.tsx
+++ src/TreeEntry.tsx
@@ -5,7 +5,7 @@ import schema from 'part:@sanity/base/schema'
 
 import 'react-sortable-tree/style.css?raw'
 import {SanityDocument} from '@sanity/client'
-import {Button, Flex} from '@sanity/ui'
+import {Button, Card, Flex} from '@sanity/ui'
 import {ChevronRightIcon} from '@sanity/icons'
 
 /**
@@ -14,19 +14,26 @@ import {ChevronRightIcon} from '@sanity/icons'
  * @TODO: make it occupy all of the space available.
  */
 const TreeEntry: React.FC<SanityDocument> = (props) => {
-  const {navigateIntent} = usePaneRouter()
+  const {navigateIntent, routerPanesState, ...rest} = usePaneRouter()
+
+  const isActive = React.useMemo(() => {
+    // If some pane is active with the current document `_id`, it's active
+    return routerPanesState.some((pane) => pane.some((group) => group.id === props._id))
+  }, [routerPanesState])
 
   return (
-    <Flex gap={2} justify="space-between" width="100%">
-      <div style={{flex: 1}}>
-        <Preview layout="default" type={schema.get(props._type)} value={{_ref: props._id}} />
-      </div>
-      <Button
-        icon={ChevronRightIcon}
-        onClick={() => navigateIntent('edit', {type: props._type, id: props._id})}
-        mode="bleed"
-      />
-    </Flex>
+    <Card tone={isActive ? 'primary' : 'default'} padding={1} radius={2}>
+      <Flex gap={2} justify="space-between" width="100%">
+        <div style={{flex: 1}}>
+          <Preview layout="default" type={schema.get(props._type)} value={{_ref: props._id}} />
+        </div>
+        <Button
+          icon={ChevronRightIcon}
+          onClick={() => navigateIntent('edit', {type: props._type, id: props._id})}
+          mode="bleed"
+        />
+      </Flex>
+    </Card>
   )
 }
 
