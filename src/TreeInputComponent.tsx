372949df391497cd78b538564477595cf8f0f523
Henrique Doro <opensource@hdoro.dev>
2021-12-03 08:53:39 -0300

    feat: wrap TreeEditor in FormField for presence & validation markers

--- src/TreeInputComponent.tsx
+++ src/TreeInputComponent.tsx
@@ -1,6 +1,7 @@
 import {FormFieldPresence} from '@sanity/base/presence'
 import {Marker, Path} from '@sanity/types'
 import React from 'react'
+import {FormField} from '@sanity/base/components'
 import TreeEditor from './components/TreeEditor'
 import {SanityTreeItem, TreeFieldSchema} from './types/types'
 
@@ -20,7 +21,15 @@ export interface TreeInputComponentProps {
 
 const TreeInputComponent: React.FC<TreeInputComponentProps> = React.forwardRef((props) => {
   return (
-    <TreeEditor options={props.type.options} tree={props.value || []} onChange={props.onChange} />
+    <FormField
+      description={props.type.description} // Creates description from schema
+      title={props.type.title} // Creates label from schema title
+      __unstable_markers={props.markers} // Handles all markers including validation
+      __unstable_presence={props.presence} // Handles presence avatars
+      compareValue={props.compareValue} // Handles "edited" status
+    >
+      <TreeEditor options={props.type.options} tree={props.value || []} onChange={props.onChange} />
+    </FormField>
   )
 })
 
