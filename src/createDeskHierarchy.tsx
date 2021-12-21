1429178b27e615ee6b1d8a2a6d4226e75467992c
Henrique Doro <opensource@hdoro.dev>
2021-12-21 14:43:25 -0300

    feat: canHandleIntent to open tree references in desk pane

--- src/createDeskHierarchy.tsx
+++ src/createDeskHierarchy.tsx
@@ -1,4 +1,5 @@
 import S from '@sanity/desk-tool/structure-builder'
+import {AddIcon} from '@sanity/icons'
 import React from 'react'
 import TreeDeskStructure from './TreeDeskStructure'
 import {TreeDeskStructureProps} from './types/types'
@@ -26,21 +27,46 @@ const deskTreeValidator = (props: TreeProps): React.FC => {
 
 export default function createDeskHierarchy(props: TreeProps) {
   const {documentId, referenceTo, referenceOptions} = props
-  const mainList =
+  let mainList = (
     referenceTo?.length === 1
-      ? S.documentTypeList(referenceTo[0]).serialize()
+      ? S.documentTypeList(referenceTo[0]).schemaType(referenceTo[0])
       : S.documentList()
-          .id(documentId)
-          .filter(referenceOptions?.filter || '')
-          .params(referenceOptions?.filterParams || {})
-          .serialize()
+  )
+    .id(documentId)
+    .menuItems(
+      (referenceTo || []).map((schemaType) =>
+        // @TODO: ensure menu items work - menuItem().action renders
+        S.menuItem()
+          .intent({
+            type: 'create',
+            params: {type: schemaType, template: schemaType, id: 'testing'}
+          })
+          // @TODO: get the title for each schema type
+          .title(schemaType)
+          .icon(AddIcon)
+      )
+    )
+    .canHandleIntent((intent: string, context: Record<string, unknown>) => {
+      if (intent === 'edit' && context.id === props.documentId) {
+        return true
+      }
+      return false
+    })
+
+  if (referenceOptions?.filter) {
+    mainList = mainList.filter(referenceOptions.filter)
+  }
+
+  if (referenceOptions?.filterParams) {
+    mainList = mainList.params(referenceOptions.filterParams)
+  }
 
   return S.listItem()
     .id(documentId)
     .title(props.title || documentId)
     .child(
       Object.assign(
-        mainList,
+        mainList.serialize(),
         {
           type: 'component',
           component: deskTreeValidator(props),
