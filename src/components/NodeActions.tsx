456b11e2d182e59e25279323781b8f2430331fd6
Henrique Doro <opensource@hdoro.dev>
2021-12-22 15:29:48 -0300

    style: scaffold for all node actions

--- src/components/NodeActions.tsx
+++ src/components/NodeActions.tsx
@@ -1,11 +1,41 @@
-import {EllipsisVerticalIcon, TrashIcon} from '@sanity/icons'
-import {Button, Menu, MenuButton, MenuItem} from '@sanity/ui'
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  EllipsisVerticalIcon,
+  LaunchIcon,
+  RemoveCircleIcon
+} from '@sanity/icons'
+import {Button, Menu, MenuButton, MenuDivider, MenuItem} from '@sanity/ui'
 import React from 'react'
 import {SanityTreeItem} from '../types/types'
 import useTreeOperations from '../utils/useTreeOperations'
 
 const NodeActions: React.FC<{item: SanityTreeItem}> = ({item}) => {
   const operations = useTreeOperations()
+
+  // Adapted from @sanity\form-builder\src\inputs\ReferenceInput\ArrayItemReferenceInput.tsx
+  // @TODO: Make OpenLink work - currently getting "No router context provider found"
+  // const OpenLink = React.useMemo(
+  //   () =>
+  //     // eslint-disable-next-line @typescript-eslint/no-shadow
+  //     React.forwardRef(function OpenLinkInner(
+  //       restProps: React.ComponentProps<typeof IntentLink>,
+  //       _ref: React.ForwardedRef<HTMLAnchorElement>
+  //     ) {
+  //       return (
+  //         <IntentLink
+  //           {...restProps}
+  //           intent="edit"
+  //           params={{id: item.node?._ref, type: item?.nodeDocType}}
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //           ref={_ref}
+  //         />
+  //       )
+  //     }),
+  //   [item.node?._ref, item?.nodeDocType]
+  // )
+
   return (
     <MenuButton
       button={<Button padding={2} mode="bleed" icon={EllipsisVerticalIcon} />}
@@ -13,10 +43,22 @@ const NodeActions: React.FC<{item: SanityTreeItem}> = ({item}) => {
       menu={
         <Menu>
           <MenuItem
-            text="Remove"
+            text="Remove from list"
             tone="critical"
-            icon={TrashIcon}
+            icon={RemoveCircleIcon}
             onClick={() => operations.removeItem(item)}
+          />
+          <MenuItem text="Move up" icon={ArrowUpIcon} onClick={() => alert('Work in progress!')} />
+          <MenuItem
+            text="Move down"
+            icon={ArrowDownIcon}
+            onClick={() => alert('Work in progress!')}
+          />
+          <MenuDivider />
+          <MenuItem
+            text="Open in new tab"
+            icon={LaunchIcon}
+            onClick={() => alert('Work in progress!')}
           />
         </Menu>
       }
