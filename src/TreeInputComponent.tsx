384a06a2ddb0d180b66f58600d48976f4b8d7e66
Christine Krüger <christine.krueger@meltingelements.com>
2023-11-06 14:46:34 +0100

    feat: treeInputComponent as FC, deleted deprecated attributes

--- src/TreeInputComponent.tsx
+++ src/TreeInputComponent.tsx
@@ -4,12 +4,9 @@ import TreeEditor from './components/TreeEditor'
 import {StoredTreeItem, TreeFieldSchema} from './types'
 import injectNodeTypeInPatches, {DEFAULT_DOC_TYPE} from './utils/injectNodeTypeInPatches'
 
-//TODO: Delete comments when Plugin is ready for production
 export interface TreeInputComponentProps {
   type: TreeFieldSchema
   value: StoredTreeItem[]
-  // compareValue: StoredTreeItem[]
-  // markers: Markers[]
   level: number
   onChange: (event: unknown) => void
   onFocus: (path: Path) => void
@@ -19,9 +16,7 @@ export interface TreeInputComponentProps {
   presence: FormNodePresence[]
 }
 
-const TreeInputComponent: React.FC<TreeInputComponentProps> = React.forwardRef((props) => {
-  // const TreeInputComponent: React.ForwardRefExoticComponent<
-  //   React.PropsWithoutRef<any> & React.RefAttributes<unknown>> = React.forwardRef((props: any) => {
+const TreeInputComponent: React.FC<TreeInputComponentProps> = (props) => {
   const documentType = props.type.options.documentType || DEFAULT_DOC_TYPE
 
   const onChange = React.useCallback(
@@ -36,13 +31,11 @@ const TreeInputComponent: React.FC<TreeInputComponentProps> = React.forwardRef((
     <FormField
       description={props.type.description} // Creates description from schema
       title={props.type.title} // Creates label from schema title
-      //__unstable_markers={props.markers} // Handles all markers including validation
       __unstable_presence={props.presence} // Handles presence avatars
-      // compareValue={props.compareValue} // Handles "edited" status
     >
       <TreeEditor options={props.type.options} tree={props.value || []} onChange={onChange} />
     </FormField>
   )
-})
+}
 
 export default TreeInputComponent
