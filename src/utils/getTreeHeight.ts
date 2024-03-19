590f3c6da2aef800870c151d5c4c25f866dd2b03
apokaliptis <apokaliptis@zoho.com>
2024-03-19 15:36:56 -0400

    fix: correctly calculate tree height

--- src/utils/getTreeHeight.ts
+++ src/utils/getTreeHeight.ts
@@ -1,10 +1,8 @@
 import {getVisibleNodeCount, TreeItem} from '@nosferatu500/react-sortable-tree'
 
-export const ROW_HEIGHT = 51
-
-export default function getTreeHeight(treeData: TreeItem[]): string {
+export default function getTreeHeight(treeData: TreeItem[], rowHeight: number): string {
   const visibleNodeCount = getVisibleNodeCount({treeData})
 
   // prettier-ignore
-  return `${50 + (ROW_HEIGHT * visibleNodeCount)}px`
+  return `${50 + (rowHeight * visibleNodeCount)}px`
 }
